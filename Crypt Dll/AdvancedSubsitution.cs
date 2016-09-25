41978aac3b34996beb71e8016977c1f5c5efe272
Henri2h <henri.carnot@hotmail.fr>
2016-09-25 16:26:21 +0200

    added my custom encription to the dll project

---
+++ Crypt Dll/AdvancedSubsitution.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crypt
+{
+    class AdvancedSubsitution
+    {
+        static int total = 256;
+
+        public static byte[] crypt_code(byte[] input_public, byte[] augmentKey)
+        {
+            int pos = 0;
+            List<Byte> output = new List<byte>();
+            foreach (byte we in input_public)
+            {
+                int output_int = defNum(Convert.ToInt32(we), augmentKey[pos]);
+                pos++;
+                if (pos >= augmentKey.Length)
+                {
+                    pos = 0;
+                }
+                output.Add(Convert.ToByte(output_int));
+            }
+            byte[] result = output.ToArray();
+            return result;
+        }
+
+        public static byte[] crypt_decode(byte[] input_public, byte[] augmentKey)
+        {
+            int pos = 0;
+            List<byte> ch_byte = new List<byte>();
+            foreach (byte byteInput in input_public)
+            {
+                int output_int = defNum_soustract(Convert.ToInt32(byteInput), augmentKey[pos]);
+                pos++;
+                if (pos >= augmentKey.Length)
+                {
+                    pos = 0;
+                }
+                ch_byte.Add(Convert.ToByte(output_int));
+            }
+            byte[] result = ch_byte.ToArray();
+
+            // ended
+            return result;
+        }
+
+        private static int defNum(int pos, int augment)
+        {
+            int result = 0;
+            if (pos < (total - augment))
+            {
+                result = pos + augment;
+            }
+            else
+            {
+                result = pos + augment - total;
+            }
+            return result;
+        }
+        private static int defNum_soustract(int pos, int augment)
+        {
+            int result = 0;
+            if (0 <= (pos - augment))
+            {
+                result = pos - augment;
+            }
+            else
+            {
+                result = pos - augment + total;
+            }
+            return result;
+        }
+    }
+}
