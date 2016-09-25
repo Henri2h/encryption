5a97103cff2c1ff6751e76239d72f509e2a7e3ae
Henri2h <henri.carnot@hotmail.fr>
2016-09-25 16:24:28 +0200

    added AES support

--- Crypt/code.cs
+++ Crypt/code.cs
@@ -56,7 +56,7 @@ namespace Code
 
 
 
-
+        // this salt should not exist, in the future, remove it
         private static readonly byte[] SALT = new byte[] { 0x26, 0xdc, 0xff, 0x00, 0xad, 0xed, 0x7a, 0xee, 0xc5, 0xfe, 0x07, 0xaf, 0x4d, 0x08, 0x22, 0x3c };
 
         public static byte[] AESEncrypt(byte[] plain, byte[] augmentKey)
